a3c2657bf9d943203607050066249da0c9b87e36
fachri achmad maulana <fachri57@yahoo.com>
2024-12-04 20:35:14 +0700

    minor change

--- src/app/_components/FormItemComponent.tsx
+++ src/app/_components/FormItemComponent.tsx
@@ -1,10 +1,8 @@
 import { FormControl, FormDescription, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Textarea } from '@/components/ui/textarea';
-import { ChangeEventHandler } from 'react'
 import { ControllerRenderProps } from 'react-hook-form';
 
 interface geminiInput {
-  onChangeHandler: ChangeEventHandler<HTMLInputElement> | undefined;
   field: ControllerRenderProps<
     {
       prompt: string;
