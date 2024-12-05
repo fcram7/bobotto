ed71e9066dd9aa93194ea3e5215d3fb91d63826b
fachri achmad maulana <fachri57@yahoo.com>
2024-12-05 17:41:58 +0700

    added loading icon

--- src/app/_components/PromptDialog.tsx
+++ src/app/_components/PromptDialog.tsx
@@ -2,13 +2,14 @@
 
 import { promptStore } from '@/utils/states/promptState';
 import { PromptCard } from './PromptCard';
+import { Loader2 } from 'lucide-react';
 
 export const PromptDialog = () => {
   const { promptList } = promptStore();
 
   return (
     <section className='prompt-dialog-section px-[4%] lg:px-[18%] h-[550px] lg:h-[410px] overflow-y-auto'>
-      <div className='prompt-dialog-content'>
+      <div className='prompt-dialog-content h-full'>
         {promptList.length > 0 ? (
           <div className='grid gap-2'>
             {promptList.map((prompt, index) => (
@@ -27,7 +28,12 @@ export const PromptDialog = () => {
             ))}
           </div>
         ) : (
-          <p>Please enter a prompt.</p>
+          <div className="flex items-center justify-center gap-4 h-full">
+            <Loader2 size={48} className='animate-spin' />
+            <p className='text-2xl lg:text-4xl'>
+              Waiting for your prompt...
+            </p>
+          </div>
         )}
       </div>
     </section>
