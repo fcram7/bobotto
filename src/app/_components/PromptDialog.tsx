51b6d2ccd2f2de91927dbb349a1ccdabb5ae885e
fachri achmad maulana <fachri57@yahoo.com>
2024-12-05 17:28:28 +0700

    fixed dialog height

--- src/app/_components/PromptDialog.tsx
+++ src/app/_components/PromptDialog.tsx
@@ -7,13 +7,22 @@ export const PromptDialog = () => {
   const { promptList } = promptStore();
 
   return (
-    <section className="prompt-dialog-section px-[4%] lg:px-[18%] h-[550px] lg:h-[370px] overflow-y-auto">
-      <div className="prompt-dialog-content">
+    <section className='prompt-dialog-section px-[4%] lg:px-[18%] h-[550px] lg:h-[410px] overflow-y-auto'>
+      <div className='prompt-dialog-content'>
         {promptList.length > 0 ? (
-          <div className="grid gap-2">
+          <div className='grid gap-2'>
             {promptList.map((prompt, index) => (
-              <div className={`flex ${index % 2 === 0 ? 'justify-start' : 'justify-end'}`} key={index} >
-                <PromptCard user={prompt.user} prompt={prompt.prompt} cardIndex={index} />
+              <div
+                className={`flex ${
+                  index % 2 === 0 ? 'justify-start' : 'justify-end'
+                }`}
+                key={index}
+              >
+                <PromptCard
+                  user={prompt.user}
+                  prompt={prompt.prompt}
+                  cardIndex={index}
+                />
               </div>
             ))}
           </div>
@@ -22,5 +31,5 @@ export const PromptDialog = () => {
         )}
       </div>
     </section>
-  )
-}
+  );
+};
