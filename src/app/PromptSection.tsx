8ac440332c958acef0471b42af7545f3b403f864
fachri achmad maulana <fachri57@yahoo.com>
2024-12-05 17:43:02 +0700

    fixd prompt section layout

--- src/app/PromptSection.tsx
+++ src/app/PromptSection.tsx
@@ -1,12 +1,13 @@
+import { PromptDialog } from './_components/PromptDialog';
 import { PromptForm } from './_components/PromptForm';
 
 export const PromptSection = () => {
   return (
-    <section className='prompt-section px-[4%] lg:px-[2%] py-48'>
-      <h1 className='prompt-section__title text-2xl lg:text-4xl font-semibold'>
-        Bobotto Generative AI
-      </h1>
+    <section className='prompt-section px-[4%] lg:px-[2%] py-12'>
       <div className='prompt-section__content mt-10'>
+        <div className='prompt-section__prompt-dialog'>
+          <PromptDialog />
+        </div>
         <PromptForm />
       </div>
     </section>
